893117914375f0a031d4693c1a8f092d8f4f231e
isaklilja <isaklilja@gmail.com>
2015-02-09 21:16:22 +0100

    added GUI

--- A4_1_isak_lund_lilja/src/GUI.java
+++ A4_1_isak_lund_lilja/src/GUI.java
@@ -10,11 +10,16 @@ import java.awt.CardLayout;
 import java.awt.FlowLayout;
 import javax.swing.JLabel;
 import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
 
 
 public class GUI extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField humanField;
+	private JTextField dogField;
 
 	/**
 	 * Launch the application.
@@ -41,10 +46,40 @@ public class GUI extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		contentPane.setLayout(null);
+		
+		JButton newHumanButton = new JButton("New human");
+		newHumanButton.setBounds(234, 58, 117, 25);
+		contentPane.add(newHumanButton);
 		
 		JLabel lblHumansAndDogs = new JLabel("Humans and dogs");
+		lblHumansAndDogs.setBounds(149, 23, 159, 23);
 		lblHumansAndDogs.setFont(new Font("Ubuntu", Font.BOLD, 19));
 		contentPane.add(lblHumansAndDogs);
+		
+		JButton buyDogButton = new JButton("Buy dog");
+		buyDogButton.setBounds(234, 97, 117, 25);
+		contentPane.add(buyDogButton);
+		
+		JButton showInfoButton = new JButton("Show info");
+		showInfoButton.setBounds(234, 134, 117, 25);
+		contentPane.add(showInfoButton);
+		
+		humanField = new JTextField();
+		humanField.setBounds(108, 61, 114, 19);
+		contentPane.add(humanField);
+		humanField.setColumns(10);
+		
+		dogField = new JTextField();
+		dogField.setBounds(108, 100, 114, 19);
+		contentPane.add(dogField);
+		dogField.setColumns(10);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(12, 192, 426, 96);
+		contentPane.add(scrollPane);
+		
+		JTextArea infoArea = new JTextArea();
+		scrollPane.setViewportView(infoArea);
 	}
 }
